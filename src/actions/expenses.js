025aaded8b1487c678042abb8c5996b6f800ec33
Steven Martins <Steven@MacBook-Pro.local>
2020-10-13 20:10:42 -0700

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,3 +43,29 @@ export const editExpense = (id, updates) => ({
   id,
   updates,
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: "SET_EXPENSES",
+  expenses,
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database
+      .ref("expenses")
+      .once("value")
+      .then((snapshot) => {
+        const expenses = [];
+
+        snapshot.forEach((childSnapShot) => {
+          expenses.push({
+            id: childSnapShot.key,
+            ...childSnapShot.val(),
+          });
+        });
+
+        dispatch(setExpenses(expenses));
+      });
+  };
+};
