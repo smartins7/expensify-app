3ff5d3ec9ce6c053a0ce60703571bde4130795db
Steven Martins <Steven@MacBook-Pro.local>
2020-10-19 16:52:20 -0700

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -10,15 +10,14 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
   if (!hasRendered) {
@@ -27,13 +26,10 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    store.dispatch(startSetExpenses()).then(() => {
-      ReactDOM.render(jsx, document.getElementById("app"));
-    });
     store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
